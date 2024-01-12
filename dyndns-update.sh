248dbc620c6fb06f681b67d1be8c1cf95a690cc7
André <dev@andre-mueller.de>
2024-01-12 20:04:27 +0100

    initial commit

---
+++ dyndns-update.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+myfritzdomain=<your myfritz subdomain>
+desecToken=<deSECToken>
+domains=('example.com' 'example.org')
+
+
+
+IPv4=$(dig @9.9.9.9 "$myfritzdomain" A +short)
+
+IPv6=$(dig @9.9.9.9 "$myfritzdomain" AAAA +short)
+
+if [ ! -e /tmp/dyndnsv4 ]; then
+    echo $IPv4 > /tmp/dyndnsv4
+fi
+
+if [ ! -e /tmp/dyndnsv6 ]; then
+    echo $IPv6 > /tmp/dyndnsv6
+fi
+
+
+if [ $(cat /tmp/dyndnsv4) != "$IPv4" ] || [ $(cat /tmp/dyndnsv6) != "$IPv6" ]; then
+    echo "$(date +"%d.%m.%Y %H:%M") The IP-Adresses have changed"
+    echo $IPv4 > /tmp/dyndnsv4
+    echo $IPv6 > /tmp/dyndnsv6
+
+    
+    for domain in "${domains[@]}"; do
+
+        echo "$(date +"%d.%m.%Y %H:%M") Sende Update für $domain"
+          StatusCode=$(curl "https://update6.dedyn.io/?hostname=$domain&myipv4=$IPv4&myipv6=$IPv6" \
+          --header "Authorization: Token "$desecToken"" \
+          -s \
+          -o /dev/null \
+          -w "%{http_code}")
+
+            if [ $StatusCode -eq 200 ]; then
+                echo "$(date +"%d.%m.%Y %H:%M") Updating the domain $domain was successful (Statuscode: $StatusCode)"
+            else
+                echo "$(date +"%d.%m.%Y %H:%M") Updating the domain $domain was not successful (Statuscode: $StatusCode)"
+            fi
+
+    done
+fi
+
+
+
